8ffa43894ff69d1024ef21335ec32686e5e42f31
Terrence Mahnken <terrence.mahnken@hcahealthcare.com>
2021-02-22 14:27:10 -0600

    completes module 3

---
+++ stories/banner.stories.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import MajorBanner from "../components/major.banner";
+import MinorBanner from "../components/minor.banner";
+
+storiesOf("Component|Banners/Major", module).add("With Only Title", () => (
+    <MajorBanner title="Banner Title" photo="People Outdoors/shutterstock_116403520.jpg" />
+)).add("With all Text Options", () => (
+    <MajorBanner photo="People Outdoors/shutterstock_116403520.jpg" title="Banner Title" subtitle="Banner Subtitle"
+    body="Banner Body"
+    />
+))
+
+storiesOf("Component|Banners/Minor", module).add("No Pictures", () => (
+    <MinorBanner
+    title="Banner Title"
+    subtitle="Banner Subtitle"
+    body="Banner Body"
+    />
+)).add("With Pictures", () => (
+    <MinorBanner
+    title="Banner Title"
+    subtitle="Banner Subtitle"
+    body="Banner Body"
+    leftPhoto="Products/boots/shutterstock_66842440.jpg"
+    rightPhoto="Products/boots/shutterstock_1121278055.jpg"
+    />
+))
---
+++ stories/button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import CallToAction from "../components/form/cta-button";
+import { action } from "@storybook/addon-actions"
+
+storiesOf("Building Blocks | Button", module).add("Call To Action", () => (
+    <CallToAction label="Submit" 
+    onClick={action("button-click")}/>
+))
